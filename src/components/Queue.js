aaaa0d95538e61adb0ca2286022b4f9b85bd4bfa
David Lougheed <david.lougheed@gmail.com>
2018-02-09 16:37:08 -0500

    Add a nonfunctional pagination UI for queue tables.

--- src/components/Queue.js
+++ src/components/Queue.js
@@ -9,7 +9,25 @@ class Queue extends React.Component {
 
 	render() {
 		return (
-			<JobTable queueName={this.props.match.params.queueName} jobs={this.props.jobs} showUser={true} />
+			<div>
+				<JobTable queueName={this.props.match.params.queueName}
+						  jobs={this.props.jobs}
+						  loading={this.props.loadingJobs}
+						  showUser={true} />
+				<div className="table-nav">
+					<div className="page-text">
+						Showing 1 to 100 of {this.props.jobs.length} jobs
+					</div>
+					<div className="page-buttons">
+						<nav className="card-tabs icons-only">
+							<ul>
+								<a><li><i className="material-icons">keyboard_arrow_left</i></li></a>
+								<a><li><i className="material-icons">keyboard_arrow_right</i></li></a>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
