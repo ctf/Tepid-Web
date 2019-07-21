5c5f95c11febad4fc61d5566bb71060735992542
Daniel Goldman <merkavabuilder@gmail.com>
2019-07-21 11:20:47 -0400

    name on header dropdown uses that of user

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,27 +1,33 @@
 import React from 'react';
+import {connect} from "react-redux"
 import {NavLink} from 'react-router-dom';
 
-class PageHeader extends React.Component {
-	render() {
-		return (
-			<header>
-				<div className="header-left">
-					<i className="material-icons">search</i>
-					<input type="text" name="user-search" id="header-user-search" placeholder="Search for users..." />
-				</div>
-				<div className="header-right">
-					<div id="header-user-dropdown">
-						David Lougheed
-						<i className="material-icons">keyboard_arrow_down</i>
-						<ul>
-							<li><NavLink to="/my-account">My Account</NavLink></li>
-							<li>Sign Out</li>
-						</ul>
-					</div>
-				</div>
-			</header>
-		);
-	}
-}
+
+const mapStateToProps = state => {
+    return {auth: state.auth}
+};
+
+const PageHeaderContent = ({auth})=> {
+    return (
+        <header>
+            <div className="header-left">
+                <i className="material-icons">search</i>
+                <input type="text" name="user-search" id="header-user-search" placeholder="Search for users..."/>
+            </div>
+            <div className="header-right">
+                <div id="header-user-dropdown">
+                    {auth.user.displayName}
+                    <i className="material-icons">keyboard_arrow_down</i>
+                    <ul>
+                        <li><NavLink to="/my-account">My Account</NavLink></li>
+                        <li>Sign Out</li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+const PageHeader = connect(mapStateToProps)(PageHeaderContent);
 
 export default PageHeader;
