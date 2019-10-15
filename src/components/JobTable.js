353d375dca7a56a4fd447e5c953bff62942a062f
Daniel Goldman <merkavabuilder@gmail.com>
2019-10-14 22:08:54 -0400

    rebuild JobTable with Ant
    everything except showUser toggle

--- src/components/JobTable.js
+++ src/components/JobTable.js
@@ -1,86 +1,61 @@
-import React, {useState} from 'react';
-
-import JobTableRow from './JobTableRow';
-import TableHead from "@material-ui/core/TableHead";
-import {TableRow} from "@material-ui/core";
-import TableCell from "@material-ui/core/TableCell";
-import TableBody from "@material-ui/core/TableBody";
-
+import React from 'react';
+import {Table} from "antd";
+import {Link} from "react-router-dom";
+import {jobStatus} from "../tepid-utils";
+import {useDispatch, useSelector} from "react-redux";
+import * as actions from "../actions";
+import {MoreMenu} from "./Buttons/MoreButton";
+import MenuItem from "@material-ui/core/MenuItem";
 
 function JobTable({loading, showUser, canRefund, jobs}) {
 
-	const [offset, setOffset] = useState(0);
-	const [pageSize, setPageSize] = useState(100);
+	const destinations = useSelector(state => state.destinations.items);
+	const getDestination = (job) => destinations[job.destination];
 
-	const handlePageBack = () => {
-		setOffset(Math.max(0, offset - pageSize));
-	};
+	const dispatch = useDispatch();
+	const makeHandleRefund = (job) => () => dispatch(actions.doSetJobRefunded(job, !job.refunded));
 
-	const handlePageForward = () => {
-		if (offset + pageSize < jobs.length) {
-			setOffset(offset + pageSize)
+	const columns = [
+		{
+			title: 'Started',
+			key: 'Started',
+			render: job => <>{job.started === -1 ? '' : new Date(job.started).toLocaleString('en-CA')}</>
+		},
+		{
+			title: 'User',
+			key: 'User',
+			render: job => <Link to={`/accounts/${job.userIdentification}`}> {job.userIdentification}</Link>
+		},
+		{
+			title: 'Pages',
+			key: 'Pages',
+			render: job => <>{`${job.pages} ${job.colorPages === 0 ? '' : ` (${job.colorPages} color)`}`}</>
+		},
+		{
+			title: 'Status',
+			key: 'Status',
+			render: job => <>{jobStatus(job)}</>
+		},
+		{
+			title: 'Destination',
+			key: 'Destination',
+			render: job => { const destination = getDestination(job); return <>{(destination && destination.name) || ""}</>}
+		},
+		{
+			title: 'Name',
+			key: 'Name',
+			render: job => <>{job.name}</>
+		},
+		{
+			title: '',
+			key: 'MoreMenu',
+			render: job => <MoreMenu>{canRefund && <MenuItem onClick={makeHandleRefund(job)}>Refund</MenuItem>}<MenuItem>Reprint</MenuItem></MoreMenu>
 		}
-	};
-
-	const colSpan = showUser ? 7 : 6;
-
-	let jobList = jobs.slice(offset, offset + pageSize)
-		.map(job => (<JobTableRow key={job._id} job={job} showUser={showUser} canRefund={canRefund}/>));
-
-	if (loading) {
-		jobList = (<tr>
-			<td colSpan={colSpan} style={{textAlign: 'center'}}>Loading...</td>
-		</tr>);
-	}
+	];
 
 	return (
 		<div>
-			<table className="job-list">
-				<TableHead>
-					<TableRow>
-						<TableCell style={{width: '20%'}}>Started</TableCell>
-						{
-							showUser
-								? (<th style={{width: '15%'}}>User</th>)
-								: ''
-						}
-						<TableCell style={{width: '10%'}}>Pages</TableCell>
-						<TableCell style={{width: '20%'}}>Status</TableCell>
-						<TableCell style={{width: '15%'}}>Destination</TableCell>
-						<TableCell style={{}}>Name</TableCell>
-						<TableCell style={{width: '6%'}}>&nbsp;</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{jobList}
-				</TableBody>
-			</table>
-			{loading
-				? ''
-				: (
-					<div className="table-nav">
-						<div className="page-text">
-						<span>
-							Showing {offset + 1} to&nbsp;
-							{Math.min(offset + pageSize, jobs.length)} of&nbsp;
-							{jobs.length} jobs
-						</span>
-						</div>
-						<div className="page-buttons">
-							{pageSize < jobs.length ? (<nav className="card-tabs icons-only small">
-								<ul>
-									<a onClick={handlePageBack}>
-										<li><i className="material-icons">keyboard_arrow_left</i></li>
-									</a>
-									<a onClick={handlePageForward}>
-										<li><i className="material-icons">keyboard_arrow_right</i></li>
-									</a>
-								</ul>
-							</nav>) : ''}
-						</div>
-					</div>
-				)
-			}
+				<Table columns={columns} dataSource={jobs} loading={loading}/>
 		</div>
 	);
 }
